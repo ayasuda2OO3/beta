fdca225e68d13fb3947d64a4ab9699edda43920f
Apolo Yasuda <apolo@Apolos-MacBook-Pro.local>
2013-01-30 15:36:36 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beta::Application.config.secret_token = 'c24d1c27586fd755a26bb773bc04e0ee0790b6237d99809037b0724565847e96c7485e41ecc69bad4b7849f055a9a782f6c47da3c2a76e9eeb503960c5892774'
